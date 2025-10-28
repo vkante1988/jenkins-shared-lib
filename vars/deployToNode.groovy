3b5ff15b0bab15a470105f2a789cce548ea38ded
vkante1988 <vkante1988@gmail.com>
2025-10-28 11:37:53 +0530

    Update deployToNode.groovy

--- vars/deployToNode.groovy
+++ vars/deployToNode.groovy
@@ -1,16 +1,12 @@
 def call(String nodeLabel, String tomcatUrl) {
-    stage("Deploy to ${nodeLabel}") {
-        agent { label nodeLabel }
-
-        steps {
-            unstash 'app-war'
-            deploy adapters: [tomcat9(
-                credentialsId: 'tomcat-credentials',
-                path: '',
-                url: "http://${tomcatUrl}/"
-            )],
-            contextPath: null,
-            war: 'target/*.war'
-        }
+    node(nodeLabel) {
+        unstash 'app-war'
+        deploy adapters: [tomcat9(
+            credentialsId: 'tomcat-credentials',
+            path: '',
+            url: "http://${tomcatUrl}/"
+        )],
+        contextPath: null,
+        war: 'target/*.war'
     }
 }
